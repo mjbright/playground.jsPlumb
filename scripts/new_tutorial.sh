87b7d1a2a8ff0733080c0aa2f4ec50d9160a6b52
Michael Bright <github.com@mjbright.net>
2018-09-06 11:25:42 +0200

    More intelligent tutorial directory naming/detection

--- scripts/new_tutorial.sh
+++ scripts/new_tutorial.sh
@@ -10,15 +10,19 @@ while true; do
     TUTORIAL="tutorial${TUT_NUM}"
     TUTORIAL_DIR=tutorials/$TUTORIAL
 
-    [ ! -d $TUTORIAL_DIR ] && break
+    #[ ! -d $TUTORIAL_DIR ] && break
+
+    #set -x
+    EXISTS=$(find tutorials/ -type d -name "tutorial${TUT_NUM}*" | wc -l)
+    #echo $EXISTS
+    [ "$EXISTS" = "0" ] && break
+
     let TUT_NUM=TUT_NUM+1
 done
 
 echo "About to create dir $TUTORIAL_DIR"
 echo "Press <return>"
 read
-
-#TUTORIAL_DIR=TUTORIAL1; mkdir -p $TUTORIAL_DIR/css; mkdir -p $TUTORIAL_DIR/js; touch $TUTORIAL_DIR/index.html; touch $TUTORIAL_DIR/css/style.css; touch $TUTORIAL_DIR/js/TUTORIAL_DIR.js; cp -a TUTORIAL_DIR.html.template $TUTORIAL_DIR/js/TUTORIAL_DIR.js
 
 #mkdir -p $TUTORIAL_DIR/
  
