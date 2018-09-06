6baa37220ab7a1f9747d6477bbb2e07a77d31dba
Michael Bright <github.com@mjbright.net>
2018-09-06 12:06:55 +0200

    Added freedevelopertutorials.com tutorial

---
+++ tutorials/tutorial1.freedevelopertutorials.com/README.md
@@ -0,0 +1,6 @@
+
+# Tutorial
+
+This tutorial example is derived from the page at:
+http://www.freedevelopertutorials.com/jsplumb-tutorial/introduction/
+
---
+++ tutorials/tutorial1.freedevelopertutorials.com/js/tutorial.js
@@ -0,0 +1,14 @@
+
+const start = () => {
+    jsPlumb.connect({
+        source:"item_left",
+        target:"item_right",
+        endpoint:"Rectangle"
+        });
+
+    jsPlumb.draggable("item_left");
+    jsPlumb.draggable("item_right");
+};
+
+jsPlumb.ready(start);
+
