402db31a14a9cde08bbba30fac1c8bd81351f602
Michael Bright <github.com@mjbright.net>
2018-09-07 17:52:34 +0200

    Experimenting with connect parameters

--- tutorials/tutorial1.freedevelopertutorials.com.draggableElements/js/tutorial.js
+++ tutorials/tutorial1.freedevelopertutorials.com.draggableElements/js/tutorial.js
@@ -3,8 +3,14 @@ const start = () => {
     jsPlumb.connect({
         source:"item_left",
         target:"item_right",
-        endpoint:"Rectangle"
-        });
+        endpoint:"Rectangle",
+
+        connector: ["Straight"],
+        anchor: ["Left", "Right"],
+        endpoint:"Dot",
+        /*
+        */
+});
 
     jsPlumb.draggable("item_left");
     jsPlumb.draggable("item_right");
